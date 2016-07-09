653b72c63dd8af67d5fed05e3ff60ee028ca2914
Joe Marzullo <jmarzu13@gmail.com>
2016-07-09 01:16:11 -0700

    A working Theramin

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,47 @@
 $(document).ready(function() {
   console.log('this is connected');
-})
+
+  var context = new AudioContext(), oscillator = null, mousedown = false;
+
+  var gainNode = context.createGain();
+
+  function calculateFrequency(mouseXPosition) {
+    var minFrequency = 20;
+    var maxFrequency = 2000;
+
+    return ((mouseXPosition / window.innerWidth) * maxFrequency) + minFrequency;
+  };
+
+  function calculateGain(mouseYPosition) {
+    var minGain = 0;
+    var maxGain = 1;
+
+    return 1 - ((mouseYPosition / window.innerHeight) * maxGain) + minGain;
+  };
+
+  $(document).mousedown(function(e){
+    console.log(e.clientX);
+    console.log(e.clientY);
+    oscillator = context.createOscillator();
+    oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.01);
+    oscillator.connect(gainNode);
+    gainNode.connect(context.destination);
+    oscillator.start(context.currentTime);
+  });
+
+  $(document).mouseup(function() {
+    if (oscillator) {
+      oscillator.stop(context.currentTime);
+      oscillator.disconnect();
+    };
+  });
+
+  $(document).mousemove(function(e) {
+    if (oscillator) {
+      oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.01);
+    };
+
+
+    gainNode.gain.setTargetAtTime(calculateGain(e.clientY), context.currentTime, 0.01);
+  });
+});
